392e4151f7b823d756f0cc2dd270a48843fce3b1
Alexa J <alexadoestuff@protonmail.com>
2021-10-01 13:09:21 -0400

    PRAC-02 | feat : infinite scrolling. note scrollY and innerHeight, documentElement.scrollHeight takes root or html?

--- app.js
+++ app.js
@@ -1,75 +1,39 @@
-const kanjiItems = [
-  {
-    "id": "0",
-    "kanji": "向き合って",
-    "meaning": "to face; to face a problem",
-    "example": "大事な問題に直接に向き合ってが必要です。",
-    "related": "逆になる"
-  },
-  {
-    "id": "1",
-    "kanji": "つくづく",
-    "meaning": "deeply; fondly",
-    "example": "時々に彼が私につくづくに眺めっていることを気づいた。",
-    "related": "注意して"
-  },
-  {
-    "id": "2",
-    "kanji": "養う",
-    "meaning": "to provide for; to maintain; to nourish",
-    "example": "お父さんは家族を養うのため一所懸命に働いている。",
-    "related": "応援する"
+const quoteLimit = 8;
+let curQuote = 0;
+let quotes;
+
+const quoteContainer = document.getElementById('quote-container');
+
+function getRangeQuotes(cur, cap, quotes) {
+  let renderedQuotes = '';
+  for(var i = cur; i < (cap + cur); i++) {
+    let quoteCard = `
+      <div class="quote col p-4">
+        <span class="author">${quotes[i].author != null ? quotes[i].author : ''}</span>
+        <span class="saying"><p>${quotes[i].text}</p></span>
+      </div>
+    `;
+    renderedQuotes += quoteCard;
   }
-]
-
-// select items with const and getElementById
-const left = document.getElementById("left-click");
-const right = document.getElementById("right-click");
-const kanjiSection = document.getElementById("kanji");
-const answers = document.getElementById("answers");
-
-const meaning = document.getElementById("meaning-field");
-const example = document.getElementById("example-field");
-const related = document.getElementById("related-field");
-
-// init index for flipping through kanji list 
-let currentItem = 0;
-
-function populateFields(item) {
-  kanjiSection.textContent = item.kanji;
-  meaning.textContent = item.meaning;
-  example.textContent = item.example;
-  related.textContent = item.related;
+  return renderedQuotes;
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-  const item = kanjiItems[0];
-  populateFields(item);
-})
+window.addEventListener('DOMContentLoaded', (event) => {
+  fetch('https://type.fit/api/quotes')
+  .then((response) => response.json())
+  .then((data) => { 
+    quoteContainer.innerHTML = getRangeQuotes(0, quoteLimit, data);
+    curQuote += 8;
+  })
+});
 
-function createRadios(array) {
-  const radioDiv = document.createElement("div");
-}
-
-// Event listeners to the buttons
-right.addEventListener('click', function() {
-  if(kanjiItems[currentItem + 1] != null) {
-    const x = kanjiItems[currentItem + 1];
-    populateFields(x);
-    currentItem = currentItem + 1;
-  } else {
-    populateFields(kanjiItems[0]);
-    currentItem = 0;
-  }
-})
-
-left.addEventListener('click', function() {
-  if(currentItem == 0) {
-    currentItem = kanjiItems.length - 1;
-    console.log(currentItem);
-    populateFields(kanjiItems[currentItem]);
-  } else {
-    currentItem = currentItem - 1;
-    populateFields(kanjiItems[currentItem]);
+window.addEventListener('scroll', () => {
+  if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+    fetch('https://type.fit/api/quotes')
+    .then((response) => response.json())
+    .then((data) => { 
+      quoteContainer.innerHTML += getRangeQuotes(curQuote, quoteLimit, data);
+      curQuote += 8;
+    })
   }
 })
