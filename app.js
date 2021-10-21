58607c971c3030eec90a2c1adfb65def9e1abbbc
Alexa J <alexadoestuff@protonmail.com>
2021-10-21 19:00:08 -0400

    nested

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const menuItems = [
 {},{},{}
 ]
+
+const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const setItems = document.querySelectorAll('.set');
+const totalColors = 4;
+
+// 1B: Hex generator
+function generateColor() {
+  let hexVal = '#';
+}
